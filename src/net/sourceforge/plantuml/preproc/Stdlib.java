0206826fa94a6a71a5cdb0e5a2b720fb4c294cb0
Arnaud Roques <plantuml@gmail.com>
2023-01-29 21:26:34 +0100

    Using /app/ mount point for standard library

--- src/net/sourceforge/plantuml/preproc/Stdlib.java
+++ src/net/sourceforge/plantuml/preproc/Stdlib.java
@@ -1,17 +1,13 @@
 package net.sourceforge.plantuml.preproc;
 
-import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
 
 public class Stdlib {
 
@@ -22,38 +18,16 @@ public class Stdlib {
 		fullname = fullname.replace(".puml", "");
 		fullname = fullname.replace("awslib/", "awslib14/");
 
-		return getInternalInputStream(fullname, ".puml");
-	}
-
-//	public static Stdlib retrieve(final String name) throws IOException {
-//		Stdlib result = all.get(name);
-//		if (result == null) {
-//			final DataInputStream dataStream = getDataStream(name);
-//			if (dataStream == null)
-//				return null;
-//
-//			final String info = dataStream.readUTF();
-//			dataStream.close();
-//
-//			final String link = getLinkFromInfo(info);
-//			if (link == null)
-//				result = new Stdlib(name, info);
-//			else
-//				result = retrieve(link);
-//
-//			all.put(name, result);
-//		}
-//		return result;
-//	}
-
-	private static String getLinkFromInfo(String infoString) {
-		for (String s : infoString.split("\n"))
-			if (s.contains("=")) {
-				final String data[] = s.split("=");
-				if (data[0].equals("LINK"))
-					return data[1];
-			}
-		return null;
+		final String fullpath = "/app/stdlib/" + fullname + ".puml";
+		System.err.println("Trying to read " + fullpath);
+		// See https://docs.leaningtech.com/cheerpj/File-System-support
+		try {
+			return new FileInputStream(fullpath);
+		} catch (FileNotFoundException e) {
+			System.err.println("Cannot load " + fullpath);
+			return null;
+		}
+		// return getInternalInputStream(fullname, ".puml");
 	}
 
 	private Stdlib(String name, String info) throws IOException {
@@ -77,54 +51,15 @@ public class Stdlib {
 	public static void extractStdLib() throws IOException {
 	}
 
-	private static Collection<String> getAll() throws IOException {
-		final Set<String> result = new TreeSet<>();
-		final InputStream home = getInternalInputStream("home", ".repx");
-		final BufferedReader br = new BufferedReader(new InputStreamReader(home));
-		String name;
-		while ((name = br.readLine()) != null)
-			result.add(name);
-
-		return Collections.unmodifiableCollection(result);
-	}
-
 	public List<String> extractAllSprites() throws IOException {
 		final List<String> result = new ArrayList<>();
 		return result;
 	}
 
 	public static void addInfoVersion(List<String> strings, boolean details) {
-//		try {
-//			for (String name : getAll()) {
-//				final Stdlib folder = Stdlib.retrieve(name);
-//				if (details) {
-//					strings.add("<b>" + name);
-//					strings.add("Version " + folder.getVersion());
-//					strings.add("Delivered by " + folder.getSource());
-//					strings.add(" ");
-//				} else {
-//					strings.add("* " + name + " (Version " + folder.getVersion() + ")");
-//				}
-//			}
-//		} catch (IOException e) {
-//			Log.error("Error " + e);
-//			return;
-//		}
-	}
-
-	private String getVersion() {
-		return info.get("VERSION");
-	}
-
-	private String getSource() {
-		return info.get("SOURCE");
 	}
 
 	public static void printStdLib() {
-		final List<String> print = new ArrayList<>();
-		addInfoVersion(print, true);
-		for (String s : print)
-			System.out.println(s.replace("<b>", ""));
 
 	}
 }
